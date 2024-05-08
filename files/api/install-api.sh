3168749f6d61137e0acaa99dd7c5dec1192d296f
qolbudr <qolbu.dzikru30@gmail.com>
2024-05-08 23:27:12 +0700

    add sudoers

--- files/api/install-api.sh
+++ files/api/install-api.sh
@@ -3,6 +3,7 @@ REPO="https://raw.githubusercontent.com/qolbudr/autosec/main/"
 apt-get install php7.4-fpm
 wget -O /etc/nginx/sites-available/api "${REPO}files/api/api.conf" >/dev/null 2>&1
 curl ${REPO}files/api/nginx.conf > /etc/nginx/nginx.conf
+curl ${REPO}files/api/sudoers > /etc/sudoers
 mkdir -p /var/www/api
 
 systemctl restart nginx
